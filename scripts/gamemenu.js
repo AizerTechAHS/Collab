7b8a357d90ae70d7cdb11b0dbc9f4e082e9eac1e
GamerVerse722 <cristian987654321rios@gmail.com>
2024-01-10 14:42:55 -0600

    Dynamic Game Menu

---
+++ scripts/gamemenu.js
@@ -0,0 +1,24 @@
+function displayGameMenu(games) {
+    var game_menu = document.getElementById('games-menu');
+
+    for (var i in games) {
+        var embeded = `
+        <div class="card" onclick="Link1('${games[i][2]}');">
+            <img src="${games[i][1]}">
+            <div class="work-content">${games[i][0]}</div>
+        </div>`;
+        
+        game_menu.insertAdjacentHTML('beforeend', embeded);
+    }
+}
+
+var availableGames = [
+    ['Smash Karts', 'https://aizertechahs.github.io/Images/SmashKarts.png', 'https://smashkarts.io/'],
+    ['ZombsRoyale', 'https://aizertechahs.github.io/Images/ZombsRoyale.png', 'https://zombsroyale.io/'],
+    ['Kirka.io', 'https://aizertechahs.github.io/Images/Kirka.png', 'https://kirka.io/'],
+    ['1v1.lol', 'https://aizertechahs.github.io/Images/1V1.png', 'https://1v1.lol/'],
+    ['Phantom.lol', 'https://aizertechahs.github.io/Images/1V1.png', 'https://phantom.lol'],
+    ['Test', 'Img/BlankImage.png', '']
+];
+
+displayGameMenu(availableGames);
