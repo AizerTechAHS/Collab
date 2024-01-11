8d2ff44ad398484c79f0e705029792022a4ba379
GamerVerse722 <cristian987654321rios@gmail.com>
2024-01-11 15:59:57 -0600

    new theme changer

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -1,16 +1,27 @@
-function changeTheme(theme) {
-    if (window.location.hostname == 'aizertechahs.github.io') {
-        indexUrl = '/Goguardian-Bypasser/';
-    
-      } else {
-        indexUrl = '../';
+function changeTheme(theme){
+    localStorage.setItem('currentTheme', theme)
+    switch(theme) {
+        case 'dark':
+            var colorTheme = ['#0b0c0f', '#22ff00', '#3a3b3a', '#000000'];
+            break;
+
+        case 'light':
+            var colorTheme = ['#f2f3f4', '#000000', '#3a3b3a', '#000000'];
+            break;
     }
-    document.getElementById('theme-stylesheet').href = `${indexUrl}style/${theme}.css`;
-    localStorage.setItem('currentTheme', theme);
+    var style = document.getElementById("root-changer");
+    var output = `
+    :root{
+        --main-background: ${colorTheme[0]};
+        --main-fonts-color: ${colorTheme[1]};
+        --main-tab-bar-color: ${colorTheme[2]};
+        --main-header-background: ${colorTheme[3]};
+        --main-font-family: 'Poppins', sans-serif;
+    }`;
+    style.innerHTML = output;
 }
 
-// Check if a theme is stored in local storage
 const storedTheme = localStorage.getItem('currentTheme');
 if (storedTheme) {
-    changeTheme(storedTheme);
+    changeTheme(localStorage.getItem('currentTheme'));
 }
