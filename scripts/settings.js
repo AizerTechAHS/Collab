fbf6c625572e99419114e5469524c5f05358cbe7
frantene <112424233+frantene@users.noreply.github.com>
2024-01-17 13:14:23 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -46,3 +46,30 @@ function disableClickoffCloak () {
     localStorage.setItem('clickoffCloak', 'disabled');
     window.location.reload();
 }
+
+function tabCloak() {
+  var newTitle = localStorage.getItem('cloakedTitle');
+  var newIcon = localStorage.getItem('cloakedIcon');
+
+  if (newTitle === null || newTitle === '') {
+    alert('No Cloak Detected. Please select one in settings.');
+  } else {
+    localStorage.setItem('tabTitle', newTitle);
+    localStorage.setItem('tabIcon', newIcon);
+    document.title = newTitle;
+    var icon = document.querySelector('link[rel="icon"]');
+    icon.setAttribute('href', newIcon);
+  }
+}
+
+  function disableTabCloak() {
+    var newTitle = 'Aizer Tech';
+    var newIcon = 'NOIMAGEHEREYET';
+  
+    localStorage.setItem('tabTitle', newTitle);
+    localStorage.setItem('tabIcon', newIcon);
+  
+    document.title = newTitle;
+    var icon = document.querySelector('link[rel="icon"]');
+    icon.setAttribute('href', newIcon);
+  }
