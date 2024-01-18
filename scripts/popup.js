eb67ded90ad2bde6a85e7e92b847acbf486a45dc
GamerVerse722 <cristian987654321rios@gmail.com>
2024-01-18 10:35:38 -0600

    reformat

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,12 @@
 function showPopup() {
-    document.getElementById('popup').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-  }
+  document.getElementById('popup').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+}
   
-  function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
-  }
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+}
   
 window.onload = showPopup;
   
