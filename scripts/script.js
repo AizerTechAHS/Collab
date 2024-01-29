a5152696138bc15f5aec6f689e890411ee6a0c11
frantene <112424233+frantene@users.noreply.github.com>
2024-01-29 08:58:58 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -45,3 +45,78 @@ function Link1(url) {
 function openSettings() {
   location.href = './Settings.html';
 }
+var submenuOpen = false;
+var submenu2Open = false;
+
+function toggleSubmenu(event) {
+  var submenu = document.querySelector('.context-submenu');
+  var submenuParent = document.querySelector('.with-submenu');
+
+  if (submenuOpen) {
+      submenu.style.display = 'none';
+      submenuParent.classList.remove('open');
+  } else {
+      submenu.style.display = 'block';
+      submenu.style.top = submenuParent.offsetTop + "px";
+      submenu.style.left = (submenuParent.offsetLeft + submenuParent.offsetWidth) + "px";
+      submenuParent.classList.add('open');
+      event.stopPropagation(); 
+  }
+
+  submenuOpen = !submenuOpen;
+}
+function toggleSubmenu2(event) {
+  var submenu2 = document.querySelectorAll('.context-submenu')[1];
+  var submenuParent2 = document.querySelectorAll('.with-submenu')[1];
+
+  if (submenu2Open) {
+      submenu2.style.display = 'none';
+      submenuParent2.classList.remove('open');
+  } else {
+      submenu2.style.display = 'block';
+      submenu2.style.top = submenuParent2.offsetTop + "px";
+      submenu2.style.left = (submenuParent2.offsetLeft + submenuParent2.offsetWidth) + "px";
+      submenuParent2.classList.add('open');
+      event.stopPropagation(); 
+  }
+
+  submenu2Open = !submenu2Open;
+}
+function showContextMenu(event) {
+  event.preventDefault();
+  var contextMenu = document.getElementById("contextMenu");
+  contextMenu.style.left = event.clientX + "px";
+  contextMenu.style.top = event.clientY + "px";
+  contextMenu.style.display = "block";
+}
+
+function hideContextMenu() {
+  var contextMenu = document.getElementById("contextMenu");
+  contextMenu.style.display = "none";
+  hideSubmenu();
+  hideSubmenu2();
+}
+
+function hideSubmenu() {
+  var submenu = document.querySelector('.context-submenu');
+  submenu.style.display = 'none';
+  submenuOpen = false;
+}
+
+function hideSubmenu2() {
+  var submenu2 = document.querySelectorAll('.context-submenu')[1];
+  submenu2.style.display = 'none';
+  submenu2Open = false;
+}
+document.addEventListener("contextmenu", showContextMenu);
+document.addEventListener("click", hideContextMenu);
+document.addEventListener("click", function(event) {
+  var contextMenu = document.getElementById("contextMenu");
+  var submenu = document.querySelector('.context-submenu');
+
+  if (!contextMenu.contains(event.target)) {
+      hideContextMenu();
+  } else if (!submenu.contains(event.target)) {
+      hideSubmenu();
+  }
+});
