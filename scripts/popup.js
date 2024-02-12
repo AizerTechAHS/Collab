8384ff9b2ada5160140229b8f4866b8567cdbbad
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-02-12 09:00:33 -0600

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,27 +7,3 @@ function closePopup() {
   document.getElementById('popup').style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
 }
-  
-document.addEventListener("DOMContentLoaded", function() {
-  const infoContainer = document.getElementById("infoContainer");
-  const infoBox = document.getElementById("infoBox");
-  const closeBtn = document.getElementById("closeBtn");
-  const infoText = infoContainer.textContent.trim();
-
-  infoContainer.addEventListener("click", function() {
-    infoBox.style.width = infoContainer.offsetWidth + "px";
-    infoBox.style.height = infoContainer.offsetHeight + "px";
-    infoBox.style.display = "block";
-  });
-
-  closeBtn.addEventListener("click", function(event) {
-    event.stopPropagation(); // Prevent the click event from bubbling up to infoContainer
-    infoBox.style.display = "none";
-  });
-
-  document.addEventListener("click", function(event) {
-    if (!infoContainer.contains(event.target) && !infoBox.contains(event.target)) {
-      infoBox.style.display = "none";
-    }
-  });
-});
