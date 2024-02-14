5746e792d4c1b917f5dcc99680ef23d371c6dea1
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-02-13 18:40:06 -0600

    Update kirkaserver.js

--- scripts/kirkaserver.js
+++ scripts/kirkaserver.js
@@ -46,3 +46,41 @@ function openUrl() {
     alert('Please select a folder and enter a value.');
   }
 }
+
+
+
+
+
+document.getElementById('checkButton').addEventListener('click', function() {
+    var url = document.getElementById('urlInput').value;
+    anotherFunction(url);
+});
+
+function anotherFunction(url) {
+    console.log("URL: " + url);
+  if ($("#activityCloak").is(":checked")) {
+    var url = $("#cloakurl").val() || "http://classroom.google.com";
+    var clasroomwindow = window.open(kirkaUrl, "ActivityCloakWindow");
+  }
+
+  var blankData = `<!DOCTYPE html>
+  <html>
+
+  <head>
+  </head>
+
+  <body>
+    <iframe src="${url}"
+        style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;">
+        Your browser doesn't support iframes
+    </iframe>
+    <script>
+        window.onbeforeunload = function () {return false;}
+    </script>
+  </body>
+  </html>`;
+
+  var ggwindow = window.open("", "Loading...", "width=1920,height=1080");
+
+  ggwindow.document.write(blankData);
+  }
