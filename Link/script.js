4e4d565392acb5877b0b468a201d4e864c5a26e2
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-02-21 13:04:16 -0600

    Update script.js

--- Link/script.js
+++ Link/script.js
@@ -1,10 +1,4 @@
-$(document).ready(function(){
-    var newWindow = window.open('', '_blank');
-    newWindow.document.write('<iframe src="https://aizertechahs.github.io/Goguardian-Bypasser/Link/var" style="width:100%;height:100%;border:none;"></iframe>');
-});
-
-
-function startLag(){
+window.onload = function(){
     var blankData = `<!DOCTYPE html>
   <html>
   
