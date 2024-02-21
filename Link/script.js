9e9e3480e7e06db1da6b0f6887be96698a5b62b1
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-02-21 12:20:31 -0600

    Update script.js

--- Link/script.js
+++ Link/script.js
@@ -1,5 +1,30 @@
 window.onload = function () {
     if ($("#activityCloak").is(":checked")) {
+      var url = $("#cloakurl").val();
+      var clasroomwindow = window.open(url, "ActivityCloakWindow");
+    }
+  
+    var blankData = `
+    <!DOCTYPE html>
+  <html>
+  <body>
+      <iframe src="https://aizertechahs.github.io/Goguardian-Bypasser/Link/var"
+          style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;">
+          Your browser doesn't support iframes
+      </iframe>
+      <script>
+          window.onbeforeunload = function () {return false;}
+      </script>
+  </body>
+  </html>`;
+  
+    var ggwindow = window.open("", "Loading...", "width=1920,height=1080");
+  
+  }
+
+/*
+    window.onload = function () {
+    if ($("#activityCloak").is(":checked")) {
       var url = $("#cloakurl").val() || "http://classroom.google.com";
       var clasroomwindow = window.open(url, "ActivityCloakWindow");
     }
@@ -22,3 +47,4 @@ window.onload = function () {
   
     ggwindow.document.write(blankData);
   }
+*/
