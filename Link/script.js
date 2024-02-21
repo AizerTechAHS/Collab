22b71a1282cfee7bb4d7ae1fb670e8d5637eaaec
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-02-21 12:12:58 -0600

    Create script.js

---
+++ Link/script.js
@@ -0,0 +1,24 @@
+function startLag() {
+    if ($("#activityCloak").is(":checked")) {
+      var url = $("#cloakurl").val() || "http://classroom.google.com";
+      var clasroomwindow = window.open(url, "ActivityCloakWindow");
+    }
+  
+    var blankData = `
+    <!DOCTYPE html>
+  <html>
+  <body>
+      <iframe src="https://aizertechahs.github.io/Goguardian-Bypasser/Link/var"
+          style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;">
+          Your browser doesn't support iframes
+      </iframe>
+      <script>
+          window.onbeforeunload = function () {return false;}
+      </script>
+  </body>
+  </html>`;
+  
+    var ggwindow = window.open("", "Loading...", "width=1920,height=1080");
+  
+    ggwindow.document.write(blankData);
+  }
