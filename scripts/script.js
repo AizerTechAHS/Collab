e65a96ac2f1a78464587b20fed2cb23667d39a18
Aizer Tech <112424233+frantene@users.noreply.github.com>
2024-03-03 22:09:11 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -91,55 +91,6 @@ document.addEventListener("click", function (event) {
     hideSubmenu();
   }
 });
-function enableAboutBlank() {
-  localStorage.setItem("aboutBlank", "enabled");
-  window.location.reload();
-}
-
-function disableAboutBlank() {
-  localStorage.setItem("aboutBlank", "disabled");
-  window.location.reload();
-}
-
-var blankState = localStorage.getItem("aboutBlank");
-
-var blankerCheck = localStorage.getItem("aboutBlank");
-if (blankerCheck === "enabled") {
-  let inFrame;
-  try {
-    inFrame = window !== top;
-  } catch (e) {
-    inFrame = true;
-  }
-  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = open("about:blank", "_blank");
-    if (!popup || popup.closed) {
-      alert("Please allow popups and redirects for about:blank cloak to work.");
-    } else {
-      const doc = popup.document;
-      const iframe = doc.createElement("iframe");
-      const style = iframe.styles;
-      const link = doc.createElement("link");
-      const name = localStorage.getItem("name") || "My Drive - Google Drive";
-      const icon =
-        localStorage.getItem("icon") ||
-        "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
-      doc.title = name;
-      link.rel = "icon";
-      link.href = icon;
-      iframe.src = location.href;
-      style.position = "fixed";
-      style.top = style.bottom = style.left = style.right = 0;
-      style.border = style.outline = "none";
-      style.width = style.height = "100%";
-      doc.head.appendChild(link);
-      doc.body.appendChild(iframe);
-      location.replace("https://annaisd.schoology.com/home");
-    }
-  }
-} else {
-  console.log("Blanker Disabled.");
-}
 
 var selectedTheme = localStorage.getItem("selectedOption");
 var bgUrl = localStorage.getItem("bgUrl");
